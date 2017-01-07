4bd6b8de80b1efd11858b7dadd45de00ac346317
Peter Lawler <relwalretep@gmail.com>
2017-01-08 00:03:13 +1100

    Attempt to update for Django 1.10 views

--- website/frontend/urls.py
+++ website/frontend/urls.py
@@ -1,23 +1,30 @@
 from django.conf.urls import url
+from frontend.views import (about, article_history, article_history_feed,
+                            browse, contact, diffview, examples, feed,
+                            front, json_view, old_diffview, press, subscribe,
+                            upvote)
 
 urlpatterns = [
   # These are deprecated, and meant to preserve legacy URLs:
-  url(r'^diffview/$', 'frontend.views.old_diffview'),
-  url(r'^article-history/$', 'frontend.views.article_history', name='article_history'),
+  url(r'^diffview/$', old_diffview),
+  url(r'^article-history/$', article_history, name='article_history'),
 
   # These are current:
-  url(r'^upvote/$', 'frontend.views.upvote', name='upvote'),
-  url(r'^diff/(?P<vid1>\d+)/(?P<vid2>\d+)/(?P<urlarg>.*)$', 'frontend.views.diffview', name='diffview'),
-  url(r'^about/$', 'frontend.views.about', name='about'),
-  url(r'^browse/$', 'frontend.views.browse', name='browse'),
-  url(r'^browse/(.*)$', 'frontend.views.browse', name='browse'),
-  url(r'^feed/browse/(.*)$', 'frontend.views.feed', name='feed'),
-  url(r'^contact/$', 'frontend.views.contact', name='contact'),
-  url(r'^examples/$', 'frontend.views.examples', name='examples'),
-  url(r'^subscribe/$', 'frontend.views.subscribe', name='subscribe'),
-  url(r'^press/$', 'frontend.views.press', name='press'),
-  url(r'^feed/article-history/(.*)$', 'frontend.views.article_history_feed', name='article_history_feed'),
-  url(r'^article-history/(?P<urlarg>.*)$', 'frontend.views.article_history', name='article_history'),
-  url(r'^json/view/(?P<vid>\d+)/?$', 'frontend.views.json_view'),
-  url(r'^$', 'frontend.views.front', name='root'),
+  url(r'^upvote/$', upvote, name='upvote'),
+  url(r'^diff/(?P<vid1>\d+)/(?P<vid2>\d+)/(?P<urlarg>.*)$',
+      diffview, name='diffview'),
+  url(r'^about/$', about, name='about'),
+  url(r'^browse/$', browse, name='browse'),
+  url(r'^browse/(.*)$', browse, name='browse'),
+  url(r'^feed/browse/(.*)$', feed, name='feed'),
+  url(r'^contact/$', contact, name='contact'),
+  url(r'^examples/$', examples, name='examples'),
+  url(r'^subscribe/$', subscribe, name='subscribe'),
+  url(r'^press/$', press, name='press'),
+  url(r'^feed/article-history/(.*)$', article_history_feed,
+      name='article_history_feed'),
+  url(r'^article-history/(?P<urlarg>.*)$', article_history,
+      name='article_history'),
+  url(r'^json/view/(?P<vid>\d+)/?$', json_view),
+  url(r'^$', front, name='root'),
 ]
